e81df060cbeffcf63ef4d0d11221012a1f459f15
langzilang <1874815111@qq.com>
2021-02-26 02:10:41 +0800

    declaration-files

---
+++ src/typescript-basic/declaration-files.ts
@@ -0,0 +1,6 @@
+// jQuery('#foo')
+
+//安装完@types/jquery, 就不需要.d.ts 里边去写第三方库了
+// npm install --seve @types/jquery --registry=https://registry.npm.taobao.org
+
+jQuery('#foo')
---
+++ vue.config.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	publicPath: './',
+	devServer: {
+		open: true,
+		// proxy:{
+		// 	'/':{
+		// 		// target:'http://otc.yafco.com:9580/vfy/vfy/InterfaceInfoController/interfaceMock?interface_url=',
+		// 		target: process.env.VUE_APP_ENV_TARGET,
+		// 		changeOrigin : true,   //允许跨域
+		// 		pathRewrite: {
+
+		// 		}
+		// 	},
+		// }
+	},
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        jQuery: "jquery",
+        $: "jquery"
+      })
+    ]
+  },
+	css:{
+		loaderOptions: {
+      // 给 stylus-loader 传递选项
+      // stylus: {
+      //   import: '~@/components/stylus/variables.styl'
+      // }
+    }
+	}
+}
