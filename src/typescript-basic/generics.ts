2baddea1b2ce340845cccc61883174367df73b8c
langzilang <1874815111@qq.com>
2021-02-26 09:41:21 +0800

    edit

--- src/typescript-basic/generics.ts
+++ src/typescript-basic/generics.ts
@@ -4,12 +4,12 @@ function echo<T>(arg: T): T {
   return arg
 }
 // const strr: string = 'str'
-const result = echo(true)
+const results = echo(true)
 
 function swap<T,U>(tuple:[T,U]) : [U,T] {
    return [tuple[1],tuple[0]]
 }
-const result2 = swap(['string',123])
+const result4 = swap(['string',123])
 
 function echoWithArr<T>(arg: T): T {
   //console.log(arr.length) //错误，在函数内部使用泛型，事先不知道类型，不能乱用属性方法
